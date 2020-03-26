a3269851c2983678f03211cc9001a47430552706
marco.allotta <marco.allotta@chili.com>
2020-03-26 13:14:32 +0100

    fix :€

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict'
+const visit = require(`unist-util-visit`)
+
+module.exports = ({ markdownAST }, options = { width: 600, height: 300 }) => {
+  function isUrlValid(userInput) {
+    var res = userInput.match(
+      /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+    )
+    if (res == null) return false
+    else return true
+  }
+  visit(markdownAST, `inlineCode`, node => {
+    const { value } = node
+
+    if (value.startsWith(`youtube:`)) {
+      const videoUrl = value.substr(8)
+
+      if (isUrlValid(videoUrl)) {
+        node.type = `html`
+        node.value = `<div class="video-container"><iframe class="video-item" src="${videoUrl}" style="position: absolute; width: 100%; top: 0; bottom: 0; left: 0; right: 0; height: 100%"></iframe></div>`
+      }
+    }
+  })
+
+  return markdownAST
+}
